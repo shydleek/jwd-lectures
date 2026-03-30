59a906d884acafd22075db59dcfa05281a78f62a
shydleek <matiksh.2002@gmail.com>
2026-03-30 17:14:51 +0300

    refactor override syntax

--- src/main/java/com/epam/jwd/model/User.java
+++ src/main/java/com/epam/jwd/model/User.java
@@ -38,7 +38,8 @@ public class User {
 		this.age = age;
 	}
 
-	@Override public String toString() {
+	@Override
+	public String toString() {
 		return "User{" +
 		"id=" + id +
 		", name='" + name + '\'' +
@@ -46,14 +47,16 @@ public class User {
 		'}';
 	}
 
-	@Override public boolean equals(Object o) {
+	@Override
+	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		User user = (User) o;
 		return age == user.age && Objects.equals(id, user.id) && Objects.equals(name, user.name);
 	}
 
-	@Override public int hashCode() {
+	@Override
+	public int hashCode() {
 		return Objects.hash(id, name, age);
 	}
 }
