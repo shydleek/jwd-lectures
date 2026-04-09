1fd340b32a6a3e35a9e745220d096d1e3a484b9c
shydleek <matiksh.2002@gmail.com>
2026-04-09 13:36:43 +0300

    add action-classes, add z-coordinate, add vector class

--- src/main/java/org/epam/jwd/model/Plain.java
+++ src/main/java/org/epam/jwd/model/Plain.java
@@ -1,38 +1,42 @@
 package org.epam.jwd.model;
 
+import org.epam.jwd.model.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
+import org.epam.jwd.model.Vector;
 
-public class Plain extends Point {
-    private double a;
-    private double b;
-    private double c;
+public class Plain {
+    private final double a; //BigDecimal
+    private final double b;
+    private final double c;
+    private final double d;
 
     private static final Logger LOG = LoggerFactory.getLogger(Plain.class);
     private static final String ERR_PLAIN = "Plain does not exists (a^2 + b^2 + c^2 >0)";
 
-    public Plain(double a, double b, double c){
+    public Plain(double a, double b, double c, double d){
         if (a != 0 && b != 0 && c != 0){
             this.a = a;
             this.b = b;
             this.c = c;
+            this.d = d;
         } else {
             LOG.error(ERR_PLAIN);
         }
     }
 
     public Plain(Point p1, Point p2, Point p3){
-        final double v_1_x = p2.getX() - p1.getX();
-        final double v_1_y = p2.getY() - p1.getY();
-        final double v_2_x = p3.getX() - p1.getX();
-        final double v_2_y = p3.getY() - p1.getY();
-        final double plain = v_1_x*v_2_y-v_1_y*v_2_x;
-        if (plain != 0) {
-            //this.a =
-            //this.b =
-            //this.c =
+        final Vector vector1 = new Vector(p1, p2);
+        final Vector vector2 = new Vector(p1, p3);
+        final Vector n = Vector.crossProduct(vector1, vector2);
+
+        if (n.getX() != 0 && n.getY() != 0 && n.getZ() != 0) {
+            this.a = (p2.getY()-p1.getY())*(p3.getZ()-p1.getZ())-(p2.getZ()-p1.getZ())*(p3.getY()-p1.getY());
+            this.b = (p2.getZ()-p1.getZ())*(p3.getX()-p1.getX())-(p2.getX()-p1.getX())*(p3.getZ()-p1.getZ());
+            this.c = (p2.getX()-p1.getX())*(p3.getY()-p1.getY())-(p2.getY()-p1.getY())*(p3.getX()-p1.getX());
+            this.d = -(a * p1.getX() + b * p1.getY() + c * p1.getZ());
         } else {
             LOG.error(ERR_PLAIN);
         }
@@ -50,17 +54,20 @@ public class Plain extends Point {
         return c;
     }
 
+    public double getD() {
+        return d;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
         Plain plain = (Plain) o;
-        return Double.compare(a, plain.a) == 0 && Double.compare(b, plain.b) == 0 && Double.compare(c, plain.c) == 0;
+        return Double.compare(a, plain.a) == 0 && Double.compare(b, plain.b) == 0 && Double.compare(c, plain.c) == 0 && Double.compare(d, plain.d) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), a, b, c);
+        return Objects.hash(a, b, c, d);
     }
 
     @Override
@@ -69,6 +76,7 @@ public class Plain extends Point {
                 "a=" + a +
                 ", b=" + b +
                 ", c=" + c +
+                ", d=" + d +
                 '}';
     }
 }
--- src/main/java/org/epam/jwd/model/Vector.java
+++ src/main/java/org/epam/jwd/model/Vector.java
@@ -1,17 +1,29 @@
 package org.epam.jwd.model;
 
-import java.math.BigDecimal;
+import java.nio.channels.Pipe;
 import java.util.Objects;
 
-public class Point {
+public class Vector {
     private final double x;
     private final double y;
     private final double z;
 
-    public Point(double x, double y, double z) {
+    public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public static Vector crossProduct(Vector a, Vector b){
+        return new Vector(a.getY() * b.getZ() - a.getZ() * b.getY(),
+                a.getZ() * b.getX() - a.getX() * b.getZ(),
+                a.getX() * b.getY() - a.getY() * b.getX());
+    }
+
+    public Vector(Point p1, Point p2) {
+        this.x = p2.getX() - p1.getX();
+        this.y = p2.getY() - p1.getY();
+        this.z = p2.getZ() - p1.getZ();
     }
 
     public double getX() {
@@ -29,8 +41,8 @@ public class Point {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0 && Double.compare(z, point.z) == 0;
+        Vector vector = (Vector) o;
+        return Double.compare(x, vector.x) == 0 && Double.compare(y, vector.y) == 0 && Double.compare(z, vector.z) == 0;
     }
 
     @Override
@@ -40,12 +52,10 @@ public class Point {
 
     @Override
     public String toString() {
-        return "Point{" +
+        return "Vector{" +
                 "x=" + x +
                 ", y=" + y +
                 ", z=" + z +
                 '}';
     }
 }
-
-
